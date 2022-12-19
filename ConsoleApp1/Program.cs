279cbf0c934bfafe84194eceb04b8a6e9f27f591
sokelva <sokelva@mail.ru>
2022-12-19 11:43:50 +0300

    Задание 6.2.2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace FirstApp
+{
+	class Pen
+	{
+		public string color;
+		public double cost;
+
+		// Конструктор 1
+		public Pen()
+		{
+			color = "Черный";
+			cost = 100;
+		}
+
+		// Конструктор 2
+		public Pen(string c, int co)
+		{
+			color = c;
+			cost = co;
+		}
+	}
+
+	class Human
+	{
+		// Поля класса
+		public string name;
+		public int age;
+
+		// Метод класса
+		public void Greetings()
+		{
+			Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+		}
+
+		// Конструктор 1
+		public Human()
+		{
+			name = "Неизвестно";
+			age = 20;
+		}
+		// Конструктор 2
+		public Human(string n)
+		{
+			name = n;
+			age = 20;
+		}
+		// Конструктор 3
+		public Human(string n, int a)
+		{
+			name = n;
+			age = a;
+		}
+	}
+
+	struct Animal
+	{
+		// Поля структуры
+		public string type;
+		public string name;
+		public int age;
+
+		// Метод структуры
+		public void Info()
+		{
+			Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Human human = new Human();
+			human.Greetings();
+
+			human = new Human("Дмитрий");
+			human.Greetings();
+
+			human = new Human("Дмитрий", 23);
+			human.Greetings();
+
+			Console.ReadKey();
+		}
+	}
+}
